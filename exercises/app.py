68df9eb099434aeb8f7430acd0129a20b50e376d
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:11:47 +0300

    s

--- exercises/app.py
+++ exercises/app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-fav_players = ["messi", "mohammad salah", "michael phelps"]
+
 def home_page():
-	return render_template("index.html")
-for i in range (3):
-	
+	fav_players = ["messi", "mohammad salah", "michael phelps"]
+	likes_same_sport = False
+	return render_template(
+		"index.html",
+		fav_players=fav_players,
+		likes_same_sport=likes_same_sport)
+
 
 
 if __name__ == '__main__':
