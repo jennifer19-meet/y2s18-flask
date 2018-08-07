6dad6bda0e886f8b4e636168278ef1011229dfc7
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 14:19:47 +0300

    I cloned the wrong one

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+fav_players = ["messi", "mohammad salah", "michael phelps"]
 def home_page():
-    pass
+	return render_template("index.html")
+for i in range (3):
+	
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
+	app.run(debug = True)
